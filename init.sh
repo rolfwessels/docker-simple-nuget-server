32a294fc7f418ccc76fccc1aa0ddd5213dfe63be
Ian <heroims@163.com>
2020-02-04 18:26:48 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ sed -i -- 's/example.com/'"$NUGET_HOST"'/g' /etc/nginx/conf.d/default.conf
 sed -i -- 's/return.*http:.*;/return "'"$NUGET_DEFAULT_HTTP"':\/\/";/g' $NUGET_PATH/inc/core.php 
 sed -i -- 's/.*upload_max_filesize.*=.*/upload_max_filesize = '$NUGET_UPLOAD_MAX_SIZE'/g' /etc/php/7.0/fpm/php.ini
 sed -i -- 's/.*post_max_size.*=.*/post_max_size = '$NUGET_UPLOAD_MAX_SIZE'/g' /etc/php/7.0/fpm/php.ini
-sed -i -- 's/.*#gzip  on;/    client_max_body_size '$NUGET_UPLOAD_MAX_SIZE';/g' /etc/nginx/nginx.conf
+sed -i -- 's/.*client_max_body_size.*;/ client_max_body_size '$NUGET_UPLOAD_MAX_SIZE';/g' /etc/nginx/nginx.conf
 
 chown www-data /app/db
 chown www-data /app/packagefiles
